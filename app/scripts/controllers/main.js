fc7276a6788a9425512778b3056493b49fd19833
allfake <allfake24@gmail.com>
2014-02-25 19:47:41 +0700

    change to use two scope

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,24 +1,32 @@
 'use strict';
 
 angular.module('astatusApp')
-  .controller('MainCtrl', function ($scope, $firebase) {
+  .controller('MainCtrl', function ($scope, $firebase, $timeout) {
 
     var serverTimeOffset = new Firebase("https://vivid-fire-1812.firebaseio.com/.info/serverTimeOffset");
-    var statusList = new Firebase("https://vivid-fire-1812.firebaseio.com/").startAt().limit(500);
-    var offsetDate = new Date().getTime();
+    var statusList = new Firebase("https://vivid-fire-1812.firebaseio.com/").endAt().limit(400);
+    var offsetDate;
     var estimatedServerTimeMs;
-    $scope.statusList = $firebase(statusList);
+
+    $scope.statusListFirebase = $firebase(statusList);
+    $scope.statusList = [];
 
     serverTimeOffset.on("value", function(snap) {
-      var offsetDate = snap.val();
+      offsetDate = snap.val();
     });
 
+    statusList.on("child_added", function (snap) {
+      $timeout(function () {
+        $scope.statusList.unshift(snap.val());
+      });
+    })
+
     $scope.addValue = function () {
-      if (!$scope.newStatus || $scope.newStatus == "") {
+      if (!$scope.newStatus || $scope.newStatus == "" || !offsetDate) {
         return;
       }
       estimatedServerTimeMs = new Date().getTime() + offsetDate;
-      $scope.statusList.$add({text: $scope.newStatus, create_at: new Date(estimatedServerTimeMs)});
+      $scope.statusListFirebase.$add({text: $scope.newStatus, createAt: new Date(estimatedServerTimeMs)});
       $scope.newStatus = "";
     }
   });
